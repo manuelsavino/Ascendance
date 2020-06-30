241955ced910d6d3ae735c623772e3e20b3d80d7
manuelsavino <manuelsavino@gmail.com>
2020-06-29 21:11:03 -0400

    load paper form script

--- src/pages/PaperForm.js
+++ src/pages/PaperForm.js
@@ -1,26 +1,34 @@
 import React, { useEffect } from "react"
 // import { Link } from "gatsby"
-import lottie from "lottie-web"
-import animation from "../images/team.json"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Hero } from "../styled/Hero"
 import Helmet from "react-helmet"
 
+const addScript = url => {
+  const script = document.createElement("script")
+  script.src = url
+  script.async = true
+  document.body.appendChild(script)
+}
+
 const PaperForm = () => {
+  useEffect(() => {
+    addScript("https://paperform.co/__embed")
+  }, [])
   return (
     <Layout>
       <SEO title="Home" />
       <Hero></Hero>
-      <div data-prefill-inherit="1" data-paperform-id="vnhsjqfa"></div>
-      <Helmet>
+      <div data-paperform-id="vnhsjqfa"></div>
+      {/* <Helmet>
         <script
           async={false}
           src="https://paperform.co/__embed"
           type="text/javascript"
         />
-      </Helmet>
+      </Helmet> */}
     </Layout>
   )
 }
