4a9e375035afbf3433f11e67c291ba6b734a0fc0
manuelsavino <manuelsavino@gmail.com>
2020-07-08 21:15:29 -0400

    json structured data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react"
 // import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,6 +11,23 @@ const IndexPage = () => {
   return (
     <Layout hero={true}>
       <SEO title="Home" />
+      <Helmet
+        script={[
+          {
+            type: "application/ld+json",
+            innerHTML: `"@context": "https://schema.org",
+        "@type": "Restaurant",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "10466 NW 31ST TERRACE",
+          "addressLocality": "Doral",
+          "addressRegion": "FL",
+          "postalCode": "33172",
+          "addressCountry": "US"
+        },`,
+          },
+        ]}
+      />
       <Hero>
         <h1>Try a class for Free</h1>
       </Hero>
