0ada1ae31c7f85384254fa33ef14a5fd4de3822d
manuelsavino <manuelsavino@gmail.com>
2020-08-06 20:47:16 -0400

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
-import React, { useEffect } from "react"
-// import { Link } from "gatsby"
-import { Helmet } from "react-helmet"
-
+import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Hero } from "../styled/Hero"
@@ -11,31 +8,9 @@ const IndexPage = () => {
   return (
     <Layout hero={true}>
       <SEO title="Home" />
-      {/* <Helmet
-        script={[
-          {
-            type: "application/ld+json",
-            innerHTML: `{"@context": "https://schema.org",
-        "@type": "LocalBusiness",
-        "name": "Ascendance Studio",
-        "telephone":"+7865717778",
-        "image": [
-          "https://www.dancedoral.com/wp-content/uploads/2017/03/Ascendance-Studio-logo1.png"],
-        "address": {
-          "@type": "PostalAddress",
-          "streetAddress": "10466 NW 31ST TERRACE",
-          "addressLocality": "Doral",
-          "addressRegion": "FL",
-          "postalCode": "33172",
-          "addressCountry": "US"
-        }},`,
-          },
-        ]}
-      /> */}
       <Hero>
         <h1>Try a class for Free</h1>
       </Hero>
-
       <DanceStyles />
       {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
     </Layout>
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,19 +1,11 @@
-import React, { useEffect } from "react"
+import React from "react"
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Hero } from "../styled/Hero"
 import ScheduleViewer from "../components/Schedule/ScheduleViewer"
 
 const Schedule = () => {
-  useEffect(() => {
-    // lottie.loadAnimation({
-    //   container: animationContainer.current,
-    //   animationData: animation,
-    // })
-  }, [])
-
   return (
     <Layout>
       <SEO title="Home" />
