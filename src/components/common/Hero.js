05210fbcc8bcbb78283fab4728ff820112540549
manuelsavino <manuelsavino@gmail.com>
2020-08-06 22:27:16 -0400

    more clean up

--- src/components/common/Hero.js
+++ src/components/common/Hero.js
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import HeroImg from "../../images/hero4.jpg"
+import HeroImg from "../../images/hero.jpg"
 
 export const Hero = styled.div`
   display: flex;
@@ -15,20 +15,18 @@ export const Hero = styled.div`
   position: relative;
   z-index: 1;
   padding-top: 200px;
-  &::before {
+  /* &::before {
     content: "";
     z-index: -1;
     position: absolute;
     top: 0;
     left: 0;
-    /* background: linear-gradient(
+    background: linear-gradient(
       rgba(204, 43, 94, 0.5) 0%,
       rgba(117, 58, 136, 0.5) 100%
-    ); */
+    );
     background: radial-gradient(transparent, rgba(0, 0, 0, 0.95));
-    /* background: radial-gradient(red, green); */
-
     width: 100%;
     height: 100%;
-  }
+  } */
 `
