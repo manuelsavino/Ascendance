9c064f300f1f7961a569acbf9d85b7793224a184
manuelsavino <manuelsavino@gmail.com>
2020-08-06 22:42:12 -0400

    attempt to fix styled components issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Hero } from "../components/common/Hero"
+import { Hero } from "../components/Hero/hero"
 import DanceStyles from "../components/DanceStyles/DanceStyles"
 
 const IndexPage = () => {
