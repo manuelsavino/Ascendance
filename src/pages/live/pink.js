7e6c2a4968c00c57889a889d2e72ffa481ad9a1e
manuelsavino <manuelsavino@gmail.com>
2020-09-03 10:39:17 -0400

    add live stream page for pink room

---
+++ src/pages/live/pink.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+import {
+  Container,
+  Button,
+  VideoContainer,
+  SingleVideoCotnainer,
+} from "../../components/common/layout"
+import { Heading } from "../../components/common/copy"
+
+const NotFoundPage = () => (
+  <Layout hidefreeflass>
+    <SEO title="Pink Room Live Stream" />
+    <Container>
+      <VideoContainer>
+        <SingleVideoCotnainer>
+          <iframe
+            src="https://player.twitch.tv/?channel=ascendancestudio&parent=http://localhost:8888/&muted=true"
+            height="720"
+            width="1280"
+            frameborder="0"
+            scrolling="no"
+            allowfullscreen="true"
+          ></iframe>
+        </SingleVideoCotnainer>
+      </VideoContainer>
+    </Container>
+  </Layout>
+)
+
+export default NotFoundPage
