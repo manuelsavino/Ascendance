927a600f42a50c52e62c1ebe6ab12e30a7521b2e
manuelsavino <manuelsavino@gmail.com>
2020-09-03 15:45:05 -0400

    modify live stream pw logic

--- src/pages/live.js
+++ src/pages/live.js
@@ -46,27 +46,38 @@ export const Button = styled.button`
     cursor: pointer;
   }
 `
+
+const LiveSubHeading = styled(SubHeading)`
+  text-align: center;
+  /* margin-top: 0; */
+  @media screen and (max-width: 768px) {
+    font-size: 1.2rem;
+    padding-bottom: 10px;
+    font-weight: 400;
+    margin-top: 40px;
+  }
+`
 const ChooseRoom = ({ selectRoom }) => (
   <>
-    <SubHeading>Please choose a room</SubHeading>
+    <LiveSubHeading>Please choose a room</LiveSubHeading>
     <div>
       <Button
         onClick={() => {
-          selectRoom("ascpinkroom")
+          selectRoom({ twitch: "ascpinkroom", name: "Pink Room" })
         }}
       >
         Pink Room
       </Button>
       <Button
         onClick={() => {
-          selectRoom("ascgreenkroom")
+          selectRoom({ twitch: "ascgreenkroom", name: "Green Room" })
         }}
       >
         Green Room
       </Button>
       <Button
         onClick={() => {
-          selectRoom("ascstudio1")
+          selectRoom({ twitch: "ascstudio1", name: "Studio 1" })
         }}
       >
         Studio 1
@@ -76,14 +87,21 @@ const ChooseRoom = ({ selectRoom }) => (
 )
 
 const LiveStream = () => {
-  const [room, setRoom] = useState("")
+  const [room, setRoom] = useState()
   const [cheetoPw, setCheetoPW] = useState("")
-
-  if (cheetoPw !== "7865717778") {
+  const possiblePw = ["7865717778", `dance${new Date().getDate() + 1}`]
+  if (possiblePw.indexOf(cheetoPw) < 0) {
     return (
       <Layout>
         <SEO title="Live Stream" />
-        <Container column flex height="30vh" pb="0">
+        <Container column flex height="40vh" pb="0">
+          <LiveSubHeading>
+            For the safety of our dancers, this section of our website is
+            password protected.
+          </LiveSubHeading>
+          <LiveSubHeading style={{ marginTop: "0", marginBottom: "20px" }}>
+            Please contact the front desk for assistance.
+          </LiveSubHeading>
           <PassWordLabel htmlFor="password">Password</PassWordLabel>
           <PassWord
             name="password"
@@ -108,11 +126,12 @@ const LiveStream = () => {
 
 const Video = ({ room }) => (
   <>
-    <SubHeading>{room}</SubHeading>
+    {console.log(room)}
+    <LiveSubHeading>{room.name}</LiveSubHeading>
     <VideoContainer>
       <SingleVideoCotnainer>
         <iframe
-          src={`https://player.twitch.tv/?channel=${room}&parent=doraldance.com&muted=true`}
+          src={`https://player.twitch.tv/?channel=${room.twitch}&parent=doraldance.com&muted=true`}
           height="720"
           width="1280"
           frameborder="0"
