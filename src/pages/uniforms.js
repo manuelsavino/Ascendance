be5681b6ee92e6cac9c494098ceb040b922ae278
manuelsavino <manuelsavino@gmail.com>
2020-09-07 18:07:08 -0400

    add uniform page

---
+++ src/pages/uniforms.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react"
+import PropTypes from "prop-types"
+import withLocation from "../components/withLocation"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const addScript = url => {
+  const script = document.createElement("script")
+  script.src = url
+  script.async = true
+  document.body.appendChild(script)
+}
+
+const Uniforms = ({ search }) => {
+  const [ready, setReady] = useState(false)
+  useEffect(() => {
+    addScript("https://paperform.co/__embed")
+
+    setTimeout(() => {
+      setReady(true)
+    }, 500)
+  }, [])
+
+  let prefill = Object.keys(search)
+    .map(property => `${property}=${search[property]}`)
+    .join("&")
+
+  if (!ready)
+    return (
+      <>
+        <Layout hero={false}>
+          <SEO title="Uniforms" />
+        </Layout>
+      </>
+    )
+
+  return (
+    <>
+      <Layout hero={false}>
+        <SEO title="Free Class" />
+        {console.log(search)}
+        <div
+          style={{ marginTop: "30px" }}
+          data-prefill={prefill}
+          data-paperform-id="companyuniforms"
+        ></div>
+      </Layout>
+    </>
+  )
+}
+
+Uniforms.propTypes = {
+  search: PropTypes.object,
+}
+
+export default withLocation(Uniforms)
