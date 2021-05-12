615138dfcc1fbd4babf348cb743ea4d3f3d9e5e3
manuelsavino <manuelsavino@gmail.com>
2021-05-11 22:44:07 -0400

    added summer camp flyers

---
+++ src/pages/summer-info.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Summer1 from "../images/summer1.jpg"
+import Summer2 from "../images/summer2.jpg"
+
+const SummerInfo = () => {
+  return (
+    <Layout hidefreeflass hero={false}>
+      <SEO title="Summer Info" />
+      <div className="flex justify-center flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-2 pt-10">
+        <img src={Summer1} alt="summer camp flyer" />
+        <img src={Summer2} alt="summer camp flyer" />
+      </div>
+    </Layout>
+  )
+}
+
+export default SummerInfo
