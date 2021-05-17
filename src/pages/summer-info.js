7fe91a9e6c56a8855f5d9a14ec3ea4eaab430555
manuelsavino <manuelsavino@gmail.com>
2021-05-17 13:00:36 -0400

    added health and safety summer page

--- src/pages/summer-info.js
+++ src/pages/summer-info.js
@@ -4,19 +4,53 @@ import Seo from "../components/seo"
 import { Button } from "../components/common/layout"
 import Summer1 from "../images/summer1.jpg"
 import Summer2 from "../images/summer2.jpg"
+import "swiper/swiper-bundle.css"
+import { Swiper, SwiperSlide } from "swiper/react"
+import Parent from "../images/parents.jpeg"
+import Studio from "../images/studio.jpeg"
+import Staff from "../images/staff.jpeg"
 
 const SummerInfo = () => {
   return (
     <Layout hidefreeflass={true} hero={false}>
       <Seo title="Summer Info" />
-      <div className="flex justify-center flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-2 pt-10">
+      <div className="container mx-auto flex justify-center flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-2 pt-10">
         <Button to="/registration?type=summer">Registration</Button>
       </div>
-      <div className="flex justify-center flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-2 pt-10">
+      <div className="container mx-auto flex justify-center flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-2 pt-10">
         <img src={Summer1} alt="summer camp flyer" />
         <img src={Summer2} alt="summer camp flyer" />
       </div>
-      <div className="flex justify-center flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-2 pt-10">
+      <div className="container mx-auto">
+        <Swiper
+          spaceBetween={10}
+          loop={true}
+          autoplay={{ delay: 3000 }}
+          centeredSlides={true}
+          breakpoints={{
+            // when window width is >= 300px
+            300: {
+              slidesPerView: 1.5,
+            },
+            // when window width is >= 768px
+            768: {
+              slidesPerView: 3,
+            },
+          }}
+          className="mt-5 mb-10"
+        >
+          <SwiperSlide>
+            <img src={Parent} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Staff} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Studio} />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="flex on justify-center flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-2 ">
         <Button to="/registration?type=summer">Registration</Button>
       </div>
     </Layout>
